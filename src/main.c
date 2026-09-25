81dda68d09b88cdadfab8d62ba00960876526f8d
Shohruhmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2026-09-25 16:47:16 +0500

    feat: add initial VGA macros

--- src/main.c
+++ src/main.c
@@ -1,3 +1,11 @@
+#define VIDEO_MEMORY (void *)0xb8000
+
+#define WHITE_ON_BLACK 0x0f
+#define GREEN_ON_BLACK 0x02
+
+#define VGA_MEM_WIDTH 80
+#define VGA_MEM_HEIGHT 25
+
 void ketrnel_main(void) {
   return;
 }
