5699a889f3468de8d8290ebe1c2538d5d639af55
Shohruhmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2026-09-25 18:16:29 +0500

    feat: add clear VGA screen

--- src/main.c
+++ src/main.c
@@ -6,6 +6,17 @@
 #define VGA_MEM_WIDTH 80
 #define VGA_MEM_HEIGHT 25
 
-void ketrnel_main(void) {
+void clear_screen() {
+  char *video_memory = VIDEO_MEMORY;
+  unsigned int i = 0;
+
+  while (i < VGA_MEM_WIDTH * VGA_MEM_HEIGHT * 2) {
+    video_memory[i] = 0;
+    video_memory[i + 1] = WHITE_ON_BLACK;
+    i += 2;
+  }
+}
+
+void kernel_main(void) {
   return;
 }
